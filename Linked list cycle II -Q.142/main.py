9b4c517f5221aa17545458e0c7b9813bd951f3f4
Mrityunjay Kumar <mrityunjay7366@gmail.com>
2026-05-13 03:03:04 +0530

    initial commit

---
+++ Linked list cycle II -Q.142/main.py
@@ -0,0 +1,36 @@
+# PROBLEM NO. :-142 (Find and return the starting Node of the cycle.)
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+
+a = Node(3)
+b = Node(2)
+c = Node(0)
+d = Node(-4)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = b
+
+head = a
+slow = head
+fast = head
+pos = 0
+
+while(fast!=None and fast.next!=None):
+    fast = fast.next.next
+    slow = slow.next
+    if(slow == fast):
+        slow = head
+        while(slow!=fast):
+            slow = slow.next
+            fast = fast.next
+            pos+=1
+        break    
+print(pos)            
+        
+
+
+    
